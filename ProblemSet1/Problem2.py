f3cd4258b95bf5e63f76c162f3bb464c5eeee5c2
NicolaGV <33188538+NicolaGV@users.noreply.github.com>
2019-09-20 15:57:20 -0400

    Add files via upload

--- ProblemSet1/Problem2.py
+++ ProblemSet1/Problem2.py
@@ -11,8 +11,9 @@ plt.xlabel('Voltage (V)')
 plt.ylabel('Temperature (K)')
 
 # Neighbours cubic spline
+interval_num = 1000
 cubic_spline = interpolate.interp1d(diodes[1], diodes[0], kind='cubic')
-x_spline = np.linspace(diodes[1,0],diodes[1,len(diodes[1])-1],1000)
+x_spline = np.linspace(diodes[1,len(diodes[1])-1], diodes[1,0], interval_num)
 y_spline = cubic_spline(x_spline)
 plt.plot(x_spline, y_spline, 'g-', label="Cubic Spline")
 plt.title("Lakeshore Diodes Voltage to Temperature")
@@ -20,3 +21,24 @@ plt.legend()
 
 plt.savefig('LakeShore_Interpolated_VoltageToTemp')
 plt.show()
+
+# Error
+x_shift = np.abs(diodes[1,len(diodes[1])-1] - diodes[1,0]) / (2.0 * interval_num) # half-step shift
+x_spline_shift = np.linspace(diodes[1, len(diodes[1])-1]+x_shift, diodes[1,0]-x_shift, interval_num-1) # shifted x values
+y_spline_shift = cubic_spline(x_spline_shift) # interpolated value shifted
+yy_spline_shift = interpolate.interp1d(x_spline_shift, y_spline_shift, kind='cubic') # interpolation of shifted interpolation
+
+# remove first and last element to be in interpolation range
+xx_diodes = np.copy(diodes[1])
+xx_diodes = np.delete(xx_diodes, 0)
+xx_diodes = np.delete(xx_diodes, len(xx_diodes)-1)
+
+# interpolated y values not shifted
+yy_diodes = yy_spline_shift(xx_diodes)
+
+err = 0
+for i in range (len(yy_diodes) - 1):
+    err += (yy_diodes[i] - diodes[0,i+1])**2
+err /= len(yy_diodes)
+err = np.sqrt(err)
+print('rms error: ' + str(err))
